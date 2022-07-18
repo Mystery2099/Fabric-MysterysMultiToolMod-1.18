765a8b65ae78b7d4119c6643b0e68d2db0bbdb6a
Mystery2099 <xxmysteryxxftb@gmail.com>
2022-07-18 11:27:21 -0400

    changed item predicate names to tools

--- src/main/java/net/Mystery2099/mystmultitoolmod/MystMultiToolModClient.java
+++ src/main/java/net/Mystery2099/mystmultitoolmod/MystMultiToolModClient.java
@@ -17,16 +17,16 @@ public class MystMultiToolModClient implements ClientModInitializer {
         for(MultiToolItem tool : ModItems.getItemList()) {
             ModelPredicateProviderRegistry.register(tool, new Identifier("animated"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.DEFAULT || !mConfig.dynamicToolModel) &&
                     mConfig.defaultToolModel == DefaultToolModel.ANIMATED ? 1 : 0);
-            ModelPredicateProviderRegistry.register(tool, new Identifier("digging"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.SHOVEL && mConfig.dynamicToolModel) ||
+            ModelPredicateProviderRegistry.register(tool, new Identifier("shovel"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.SHOVEL && mConfig.dynamicToolModel) ||
                     (mConfig.defaultToolModel == DefaultToolModel.SHOVEL &&
                             (tool.toolMode == ToolModes.DEFAULT || !mConfig.dynamicToolModel)) ? 1 : 0);
-            ModelPredicateProviderRegistry.register(tool, new Identifier("mining"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.PICKAXE && mConfig.dynamicToolModel) ||
+            ModelPredicateProviderRegistry.register(tool, new Identifier("pickaxe"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.PICKAXE && mConfig.dynamicToolModel) ||
                     (mConfig.defaultToolModel == DefaultToolModel.PICKAXE &&
                             (tool.toolMode == ToolModes.DEFAULT || !mConfig.dynamicToolModel)) ? 1 : 0);
-            ModelPredicateProviderRegistry.register(tool, new Identifier("chopping"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.AXE && mConfig.dynamicToolModel) ||
+            ModelPredicateProviderRegistry.register(tool, new Identifier("axe"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.AXE && mConfig.dynamicToolModel) ||
                     (mConfig.defaultToolModel == DefaultToolModel.AXE &&
                             (tool.toolMode == ToolModes.DEFAULT || !mConfig.dynamicToolModel)) ? 1 : 0);
-            ModelPredicateProviderRegistry.register(tool, new Identifier("hoeing"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.HOE && mConfig.dynamicToolModel) ||
+            ModelPredicateProviderRegistry.register(tool, new Identifier("hoe"), (stack, world, entity, seed) -> (tool.toolMode == ToolModes.HOE && mConfig.dynamicToolModel) ||
                     (mConfig.defaultToolModel == DefaultToolModel.HOE &&
                             (tool.toolMode == ToolModes.DEFAULT || !mConfig.dynamicToolModel)) ? 1 : 0);
         }
