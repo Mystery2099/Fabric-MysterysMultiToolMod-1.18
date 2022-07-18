4f723a608ca1660a23832a1e1868089b66854e39
Mystery2099 <xxmysteryxxftb@gmail.com>
2022-07-18 10:59:33 -0400

    removed commented out old config options

--- src/main/java/net/Mystery2099/mystmultitoolmod/config/ModConfig.java
+++ src/main/java/net/Mystery2099/mystmultitoolmod/config/ModConfig.java
@@ -9,22 +9,6 @@ import net.Mystery2099.mystmultitoolmod.config.enums.ToolControls;
 
 @Config(name = MystMultiToolMod.MOD_ID)
 public class ModConfig implements ConfigData {
-    //Old Config
-    //@ConfigEntry.Category("multiToolModel")
-    //public boolean animatedToolModel = false;
-    //@ConfigEntry.Category("multiToolModel")
-    //public boolean dynamicToolModel = true;
-    //
-    //@ConfigEntry.Category("multiToolFunctionality")
-    //public boolean stripping = true;
-    //@ConfigEntry.Category("multiToolFunctionality")
-    //public boolean tilling = true;
-    //@ConfigEntry.Category("multiToolFunctionality")
-    //public boolean flattening = true;
-    //@ConfigEntry.Category("multiToolFunctionality")
-    //public boolean SwapShiftRightClickAndRightClickFunctions = false;
-
-
     //New Config
     @ConfigEntry.Category("multiToolModel")
     @ConfigEntry.Gui.Tooltip()
@@ -43,10 +27,9 @@ public class ModConfig implements ConfigData {
     public ToolControls flattening = ToolControls.SHIFT_RIGHT_CLICK;
 
     //TO-DO LIST
-    //Add config stuff to en_us.json
     //Test on Server
     //Add advancements
     //Change Predicate names to tool names
-    //Possibly add configurable durability-Lowest is default, highest is 5 times default or give only 2 options which would be 5 times default
+    //Possibly add configurable durability-Lowest is default, highest is 4 times default or give only 2 options which would be 4 times default
     //Possibly add configurable tool attack speed and damage
 }
